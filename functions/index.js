f0b6d812751f52a665db269b1b489f6902758ca4
afirstenberg <github@addventure.com>
2018-01-30 19:28:19 -0500

    Simplest implementation

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+
+const Config = require('multivocal/src/config-simple')({
+  Local: {
+    und: {
+      Response: {
+        Default: [
+          {
+            Template: "Hello World.",
+            ShouldClose: true
+          }
+        ]
+      }
+    }
+  }
+});
+
+const Multivocal = require('multivocal');
+Multivocal.setConfig( Config );
+
+exports.webhook = functions.https.onRequest( (req,res) => {
+    Multivocal.process( req, res );
+});
